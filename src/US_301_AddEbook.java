24a758c952ec1410d86d114dd9b7b0f208dda085
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-03-20 00:47:57 +0300

    add a new test case to US_301

--- src/US_301_AddEbook.java
+++ src/US_301_AddEbook.java
@@ -1,2 +1,19 @@
-public class US_301_AddEbook {
+import Utility.BaseDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class US_301_AddEbook extends BaseDriver {
+
+    @Test(priority = 1)
+    public void accessTheWebsite(){
+
+        String pageTitle=driver.getTitle();
+        Assert.assertEquals(pageTitle, "E-junkie", "Page title does not match expected value");
+
+    }
+
+    @Test(priority = 2)
+    public void addEbookToBasket (){
+
+    }
 }
