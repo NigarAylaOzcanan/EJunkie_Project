b62770e8fa4a4a8dbf9a11995db2dd43ce669e30
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-03-22 01:58:02 +0300

    Added new tests

--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
@@ -13,14 +14,20 @@ public class BaseDriver {
     public static Logger logger= LogManager.getLogger();
     public static WebDriver driver;
     public static WebDriverWait wait;
+    public static Elements elements;
+    public static Actions actions;
+
     @BeforeClass
     public void setUp() {
 
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20));
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        elements=new Elements();
+        actions=new Actions(driver);
+
         Tools.wait(2);
         NavigateToWebsite();
     }
@@ -32,16 +39,20 @@ public class BaseDriver {
         System.out.println("Test finished");
 
     }
+
     @AfterClass
     public static void tearDown() {
         Tools.wait(3);
         driver.quit();
         logger.info("Driver Closed");
     }
+
+
     @BeforeMethod
     public void BeforeMetod(ITestResult result){
         logger.info("Test Method Started: " + result.getName());
     }
+
     @AfterMethod
     public void AfterMetod(ITestResult result){
         logger.info( "The Method finished: " + result.getName() + " \n The Method result : " + (result.getStatus() == 1? "Passed" : "Failed"));
